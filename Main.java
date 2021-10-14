5360f293f5b190fd4b8da599e1edd79f63ceb929
joanaClase <joana.rente6@tuta.io>
2021-10-14 09:08:51 +0200

    Ejercicio 6

--- Main.java
+++ Main.java
@@ -9,8 +9,7 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		//ejercicio3();
-		ejercicio4();
+		ejercicio6();
 	}
 
 	//https://www2.math.upenn.edu/~kazdan/202F09/sqrt.pdf
@@ -93,6 +92,44 @@ public class Main {
 			return (input);
 		}
 		return -1;
+	}
+	
+	public static void ejercicio6() {
+		String nombre, nombreMax = "";
+		int pulsRepos, pulsPost1, pulsPost2, recuPrcnt;
+		int recuMax = 0;
+		boolean condicion = false;
+		do {
+			System.out.println("Introduce nombre atleta:");
+			nombre = sc.next();
+			if(nombre.equals("salir")) {
+				condicion = true;
+				break;
+			}
+			System.out.println("Introduce pulsaciones en reposo");
+			pulsRepos = sc.nextInt();
+			System.out.println("Introduce pulsaciones después de hacer ejercicio");
+			pulsPost1 = sc.nextInt();
+			System.out.println("Introduce pulsaciones 3 minutos después de hacer ejecercicio");
+			pulsPost2 = sc.nextInt();
+			recuPrcnt = (pulsRepos*100)/pulsPost2;
+			if(recuPrcnt >= 100) {
+				recuPrcnt = 100;
+				System.out.println("Atleta en plena forma");
+			} else if(recuPrcnt >= 50) {
+				System.out.println("Le atleta está en plena forma");
+			} else {
+				System.out.println("Le atleta necesita revisión médica");
+			}
+			System.out.println("La recuperación de "+nombre+" es de "+recuPrcnt+"%");
+			if(recuPrcnt > recuMax) {
+				recuMax = recuPrcnt;
+				nombreMax = nombre;
+			}
+		} while(!condicion);
+		if(!nombreMax.equals(""))
+			System.out.println("Mejor atleta: "+nombreMax+" con una "
+				+ "recuperación del "+recuMax+"%");
 	}
 
 }
